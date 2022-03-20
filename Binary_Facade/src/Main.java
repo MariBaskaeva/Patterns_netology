9c0a982a1761aba91b3fe52dfdea4b88e72274f4
MariBaskaeva <mariab1@list.ru>
2022-03-20 23:29:35 +0300

    Binary Facade added

---
+++ Binary_Facade/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+
+        System.out.println(bins.sum("01", "10")); //1 + 2
+        System.out.println(bins.mult("10", "101")); //2 * 5
+    }
+}
